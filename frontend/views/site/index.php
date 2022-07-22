0a46542a487891f477f6f2351db1c59285b9a553
Rafael <rkh15@hotmail.com>
2022-07-22 18:29:43 -0300

    Início

---
+++ frontend/views/site/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Busca CEP';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+        <div class="row">
+            <div class="busca_cep col-md-6">
+                <div class="row">
+                    <h2>Heading</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur.</p>
+
+                    <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
